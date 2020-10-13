a050c59e7757eb52156a4a5c3c3cb707619567f7
NIKOLAI POMARES JIMÉNEZ <npj6@clL029I-8>
2020-10-13 18:11:39 +0200

    added p2

---
+++ p2/javap2/Main.java
@@ -0,0 +1,17 @@
+package javap2;
+
+
+public class Main {
+
+  private class Hilo extends Thread {
+    
+  }
+
+  private class Ejecutable implements Runnable {
+    public void run() {}
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hewwo :3c");
+  }
+}
