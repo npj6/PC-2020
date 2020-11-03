4169e8a8ffb57ef835a3561023d0b4f50527bd8b
NIKOLAI POMARES JIMÉNEZ <npj6@clL029I-15>
2020-11-03 18:50:06 +0100

    examples written

---
+++ p3/java_p3/b.java
@@ -0,0 +1,33 @@
+import java.lang.Math; // para random
+
+public class b extends Thread {
+  static int n = 1;
+  static volatile int C[] = {0, 0};
+  int id1; // identificador del hilo
+  int id2; // identificador del otro hilo
+
+  public void run() {
+    try {
+      for(;;){
+        while(C[id2] == 1);
+        C[id1] = 1;
+        sleep((long)(100*Math.random()));
+        n = n + 1;
+        System.out.println("En hilo "+id1+", n = "+n);
+        C[id1] = 0;
+      }
+    } catch (InterruptedException e) { return; }
+
+  b(int id) {
+    this.id1 = id;
+    this.id2 = (id == 1)? 0 : 1;
+  }
+
+  public static void main(String args[]) {
+    Thread thr1 = new b(0);
+    Thread thr2 = new b(1);
+
+    thr1.start();
+    thr2.start();
+  }
+}
